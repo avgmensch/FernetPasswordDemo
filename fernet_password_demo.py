d209fa982968b4642a6553df323bcca3d3f273de
avgmensch <124395199+avgmensch@users.noreply.github.com>
2024-05-11 17:55:46 +0200

    Encrypt and decrypt message with different Fernet instances

--- fernet_password_demo.py
+++ fernet_password_demo.py
@@ -1,8 +1,9 @@
 from os import urandom, mkdir
 from os.path import exists, dirname, isfile
 from base64 import urlsafe_b64encode
+from time import time
 
-# from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.hashes import SHA256
 
@@ -42,8 +43,31 @@ key-derivation-functions/#pbkdf2)-key derivation function, later used for
 
 
 if __name__ == "__main__":
-    passwd = b"password"
+    # Input parameters
+    PASSWORD = b"password"
+    MSG_CLEAR_TEXT = b"This is my secret message..."
+
+    # Generate or get salt
     salt = get_salt()
-    kdf = get_kdf(salt)
-    key = urlsafe_b64encode(kdf.derive(passwd))
-    print(key)
+
+    # Create Fernet No. 1
+    kdf1 = get_kdf(salt)
+    key1 = urlsafe_b64encode(kdf1.derive(PASSWORD))
+    cipher1 = Fernet(key1)
+
+    # Encrypt and save message
+    msg_cipher1_enc = cipher1.encrypt(MSG_CLEAR_TEXT)
+    with open(f"{MSG_FOLDER}{time()//1}.txt.lock", "wb") as f:
+        f.write(msg_cipher1_enc)
+
+    # Create Fernet No. 2
+    kdf2 = get_kdf(salt)
+    key2 = urlsafe_b64encode(kdf2.derive(PASSWORD))
+    cipher2 = Fernet(key2)
+
+    # Decrypt and save message (new Fernet)
+    msg_cipher2_dec = cipher2.decrypt(msg_cipher1_enc)
+    print(f"Original and decoded message{" dont't " if MSG_CLEAR_TEXT !=
+                                         msg_cipher2_dec else " "}match.")
+    with open(f"{MSG_FOLDER}{time()//1}.txt", "wb") as f:
+        f.write(msg_cipher2_dec)
